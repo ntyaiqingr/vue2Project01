eb59853c9510597c2133ec2b9b3d699a2d0ee367
ntyaiqingr <tongyangni1228@gmail.com>
2022-05-06 22:55:15 +0800

    cli anaylsis

---
+++ vue2project/src_cli_anaylsis/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <SchoolInfo></SchoolInfo>
+        <StudentInfo></StudentInfo>
+    </div>
+</template>
+
+<script>
+    import SchoolInfo from "./components/SchoolInfo.vue"
+    import StudentInfo from './components/StudentInfo.vue'
+
+    export default {
+        name: "App",
+        components: {
+            SchoolInfo,
+            StudentInfo
+        }
+    }
+</script>
--- vue2project/vue.config.js
+++ vue2project/vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  // 更改入口
+/*   pages: {
+    index: {
+      entry: 'src/nick.js'
+    }
+  } */
+  lintOnSave: false // 关闭语法检查
+}
